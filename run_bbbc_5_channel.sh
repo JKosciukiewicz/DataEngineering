b7418c17c9b774909de0d14a10a44c450ee00a5d
JKosciukiewicz <jakub.kosciukiewicz@student.uj.edu.pl>
2025-08-13 18:11:44 +0200

    added bbbc channel combine

---
+++ run_bbbc_5_channel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --job-name=conf_bbbc_train
+#SBATCH --time=24:00:00
+#SBATCH --account=plgomenn-gpu-a100
+#SBATCH --partition=plgrid-gpu-a100
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --gres=gpu
+
+cd $HOME/DataEngineering
+source .venv/bin/activate
+python ./bbbbc_5channel.py
+
